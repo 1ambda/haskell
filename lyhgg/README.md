8de3d8835cf4a3c257ce7391740a48bc5992815b
1ambda <1amb4a@gmail.com>
2015-09-12 09:37:35 +0900

    configure cabal-sandbox

---
+++ lyhgg/README.md
@@ -0,0 +1,7 @@
+# Learn You a Haskell for Great Good
+
+## Project Configuration
+
+- http://mike-neck.hatenadiary.com/entry/2015/05/04/111639
+
+
---
+++ lyhgg/dist/build/autogen/Paths_lyhgg.hs
@@ -0,0 +1,36 @@
+module Paths_lyhgg (
+    version,
+    getBinDir, getLibDir, getDataDir, getLibexecDir,
+    getDataFileName, getSysconfDir
+  ) where
+
+import qualified Control.Exception as Exception
+import Data.Version (Version(..))
+import System.Environment (getEnv)
+import Prelude
+
+catchIO :: IO a -> (Exception.IOException -> IO a) -> IO a
+catchIO = Exception.catch
+
+
+version :: Version
+version = Version {versionBranch = [1,0], versionTags = []}
+bindir, libdir, datadir, libexecdir, sysconfdir :: FilePath
+
+bindir     = "/Users/1002471/Library/Haskell/bin"
+libdir     = "/Users/1002471/Library/Haskell/ghc-7.8.3-x86_64/lib/lyhgg-1.0"
+datadir    = "/Users/1002471/Library/Haskell/share/ghc-7.8.3-x86_64/lyhgg-1.0"
+libexecdir = "/Users/1002471/Library/Haskell/libexec"
+sysconfdir = "/Users/1002471/Library/Haskell/etc"
+
+getBinDir, getLibDir, getDataDir, getLibexecDir, getSysconfDir :: IO FilePath
+getBinDir = catchIO (getEnv "lyhgg_bindir") (\_ -> return bindir)
+getLibDir = catchIO (getEnv "lyhgg_libdir") (\_ -> return libdir)
+getDataDir = catchIO (getEnv "lyhgg_datadir") (\_ -> return datadir)
+getLibexecDir = catchIO (getEnv "lyhgg_libexecdir") (\_ -> return libexecdir)
+getSysconfDir = catchIO (getEnv "lyhgg_sysconfdir") (\_ -> return sysconfdir)
+
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName name = do
+  dir <- getDataDir
+  return (dir ++ "/" ++ name)
